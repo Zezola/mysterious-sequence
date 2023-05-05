ea776974370b999a19a69a691faab6f831c5bf49
Jose Ulisses <joseulisses03@gmail.com>
2023-05-04 23:03:58 -0300

    implementando criar nova linha ao clicar no botao

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
+// TO-DO: If i click in one of the buttons, the next row should have the display property changed to: grid 
 
-var random_sequence = []
-for(let i = 0; i < 5; i++) {
-    mysterious_number = Math.floor((Math.random() * 5)+1)
-    console.log(mysterious_number in random_sequence)
-    if (mysterious_number in random_sequence) {
-        mysterious_number = Math.floor((Math.random() * 5)+1)
-    } else {
-        random_sequence.push(mysterious_number)
-    }
-    
+// Click event-handler
+document.getElementById('btn').onclick = () => {
+    let new_div = render_new_div()
+    new_div.className = "guess-input"
+    document.getElementsByClassName("guesses").item(0).appendChild(new_div)
 }
-console.log(random_sequence)
-console.log("------------------------")
-let n = [1,3,5,2]
-console.log(1 in n)
+
+const render_new_div = () => {
+    let new_div = document.createElement('div')
+    // Generate the five inputs and the button
+    new_div.appendChild(document.createElement('input'))
+    new_div.appendChild(document.createElement('input'))
+    new_div.appendChild(document.createElement('input'))
+    new_div.appendChild(document.createElement('input'))
+    new_div.appendChild(document.createElement('input'))
+    new_div.appendChild(document.createElement('button'))
+    new_div.lastChild.id = 'btn'
+    console.log(new_div.lastChild)
+    return new_div    
+}
