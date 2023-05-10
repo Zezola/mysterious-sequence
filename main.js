f462df0159b3d07438049b871776fc8aaae5faf1
Jose Ulisses <joseulisses03@gmail.com>
2023-05-10 20:02:59 -0300

    button event listener for generating new div on click

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-// TO-DO: This only works for the first button. If i click on the others nothing happens.
 
-let buttons = document.getElementsByTagName('button')
-let last_button = buttons.item(buttons.length-1)
-
-last_button.onclick = () => {
+document.getElementById('btn').addEventListener('click', () => {
     let new_div = render_new_div()
-    new_div.className = "guess-input"
-    document.getElementsByClassName("guesses").item(0).appendChild(new_div)    
-}
+    document.getElementsByClassName('guesses').item(0).appendChild(new_div)
+})
 
 const render_new_div = () => {
     let new_div = document.createElement('div')
@@ -17,7 +12,6 @@ const render_new_div = () => {
     new_div.appendChild(document.createElement('input'))
     new_div.appendChild(document.createElement('input'))
     new_div.appendChild(document.createElement('input'))
-    new_div.appendChild(document.createElement('button'))
-    new_div.lastChild.id = 'btn'
+    new_div.className = 'guess-input'
     return new_div    
 }
