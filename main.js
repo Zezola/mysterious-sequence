c86a1bbf61c739d74b16d9b0d29f440f2f9b16c0
Jose Ulisses <jose_ulisses@sicredi.com.br>
2023-05-16 22:48:53 -0300

    disabling button on click

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var buttons = document.getElementsByTagName("button")
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function (e) {
         alert(this.id[this.id.length-1])
+        document.getElementById(this.id).disabled = true
         const button_clicked_id = this.id[this.id.length-1]
         let row_id = get_row_id(button_clicked_id)
         let user_input = get_user_input_per_row(row_id)
