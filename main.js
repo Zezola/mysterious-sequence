acbc5670012df9eefffd1e88413146e1b394af55
Jose Ulisses <joseulisses03@gmail.com>
2023-05-21 13:07:33 -0300

    test my commit

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 document.getElementById('btn').addEventListener('click', () => {
     let counter = 2
@@ -36,61 +35,4 @@ let get_user_sequence = () => {
         user_sequence_map.set(index,element.value)
     })
     return user_sequence_map
-=======
-var buttons = document.getElementsByTagName("button")
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = function (e) {
-        alert(this.id[this.id.length-1])
-        document.getElementById(this.id).disabled = true
-        const buttonClickedId = this.id[this.id.length-1]
-        let rowId = getRowId(buttonClickedId)
-        let userInputArray = getUserInputPerRow(rowId)
-        compareUserSequence(userInputArray)
-    }
-}
-
-function getRowId(buttonId) {
-    return "row-"+buttonId
-}
-
-console.log("JUST A TEST")
-
-function populateInputValuesIntoArray(rowId) {
-    const inputValues = []
-    const rowInputs = document.getElementById(rowId).children
-    for (let i = 0; i < rowInputs.length; i++) {
-        if (rowInputs.item(i).tagName === "INPUT") {
-            inputValues.push(rowInputs.item(i).value)
-        }
-    }
-    return inputValues
-}
-
-function getUserInputPerRow(rowId) {
-    let rowElemetsCollection
-    switch(rowId) {
-        case "row-1": 
-            rowElemetsCollection = populateInputValuesIntoArray(rowId)
-            return rowElemetsCollection
-        case "row-2":
-            rowElemetsCollection = populateInputValuesIntoArray(rowId)
-            return rowElemetsCollection
-        case "row-3":
-            rowElemetsCollection = populateInputValuesIntoArray(rowId)
-            return rowElemetsCollection
-        case "row-4":
-            rowElemetsCollection = populateInputValuesIntoArray(rowId)
-            return rowElemetsCollection
-        default:
-            return []
-    }
-}
-
-function compareUserSequence(userInputSequence) {
-    // at first lets just use a mock sequence 
-    const sequence = [1,2,3,4,5]
-    Array.from(userInputSequence).forEach((e,index)=> {
-        
-    })
->>>>>>> 78a07ffafdf123a495f67d5076b658020eb394ca
 }
